48badcf9b24e0651fd672367ee69cb15c0dd2887
Sunzhengmao <Sunzhengmao@buaa.edu.cn>
2019-10-12 23:22:36 +0800

    对第617题进行改进

--- 617_sum_bintree/sum_bintree.cpp
+++ 617_sum_bintree/sum_bintree.cpp
@@ -29,12 +29,18 @@ public:
         // }
 
         //这次试一下新建Node的方法
-        if(t1==NULL && t2==NULL) 
+        if(t1==NULL && t2==NULL)//如果全为空就返回空
             return NULL;
-        if(t1==NULL || t2==NULL) 
+        if(t1==NULL || t2==NULL)//如果有一个为空没关系，加完之后返回就行
+            return t1 ? t1 : t2 ;
+        else
         {
-            
+            TreeNode* t3 = new TreeNode(t1->val + t2->val);//如果是node指针就需要new一些，如果只是node就直接构造好了
+            t3->left = mergeTrees(t1->left, t2->left);
+            t3->right = mergeTrees(t1->right, t2->right);
+            return t3;
         }
+        
         
             
         
