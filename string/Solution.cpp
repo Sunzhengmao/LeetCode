60beea0c1444a1075fdc70b753236efa84bc6a52
Sunzhengmao <Sunzhengmao@buaa.edu.cn>
2019-11-25 23:50:17 +0800

    2019.11.25已经很久没有写力扣了，这几天在写机器视觉大作业么么哒

--- string/Solution.cpp
+++ string/Solution.cpp
@@ -125,13 +125,67 @@ public:
 
 //=============================================================================================================
     //394、编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
-    string decodeString(string s) 
+    string _394_decodeString(string s) 
     {
-        for (int i = 0; i < count; i++)
+        int len = s.size();
+        int num = 0;
+        stack<int> numstack;
+        stack<string> strstack;
+        string cur = "";
+        string result = "";
+        for(int i=0; i<len; ++i)
         {
-            /* code */
+            if(s[i]>='0' && s[i]<='9')
+            {
+                num = 10*num + s[i] - '0';
+            }
+            else if(s[i] == '[')
+            {
+                numstack.push(num);
+                strstack.push(cur);
+                num = 0;
+                cur.clear();
+            }
+            else if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z'))
+                cur += s[i];
+            else if(s[i] == ']')
+            {
+                int k = numstack.top();
+                numstack.pop();
+                for(int j=0; j<k; ++j)
+                    strstack.top() += cur;
+                cur = strstack.top();
+                strstack.pop();
+            }
         }
-        
+        result = result + cur;
+        return result;     
+    }
+
+//========================================================================================================
+    //443、给定一组字符，使用原地算法将其压缩。
+    int _443_compress(vector<char>& chars) 
+    {
+        stack<char> tmp;
+        int num=0;
+        for (vector<char>::iterator iter = chars.begin(); iter != chars.end(); iter++)
+        {
+            if(tmp.empty())
+            {
+                tmp.push(*iter);
+                continue;
+            }   
+
+            if(tmp.top() == (*iter))
+            {
+                num++;
+                iter = chars.erase(iter);
+                continue;
+            }
+            char num_char = num + '0';
+            chars.insert(iter, num_char);
+            num=0;
+        }
         
     }
 };
@@ -157,6 +211,14 @@ int main()
     //test 1221
     string s1221 = "LRLRLR";
     int output1221 = solution->_1221_balancedStringSplit(s1221);
+
+    //test 394
+    string tack = "hhh";
+    string t = "sss";
+    string a = tack + t[0];
+    string s394 = "3[a]2[bc]";
+    string s394_inner = "3[ab2[cd2[e]]]mm";
+    string output394 = solution->_394_decodeString(s394_inner);
 
 
     return 1;
