60dca822aceeec82e3f75bdb26aa063644c37c2b
Sunzhengmao <Sunzhengmao@buaa.edu.cn>
2019-12-16 23:10:31 +0800

    2019.12.16

--- binaryTree/Solution.cpp
+++ binaryTree/Solution.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <map>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 // Definition for a binary tree node.                          
@@ -196,27 +197,113 @@ public:
             return root->left->val + _404_sumOfLeftLeaves(root->left) + _404_sumOfLeftLeaves(root->right);
         return _404_sumOfLeftLeaves(root->right) + _404_sumOfLeftLeaves(root->left);
     }
+
+//==============================================================================================================
+    //236、给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+    //(1)先找出所有的路径，再找符合条件的
+    TreeNode* _236_lowestCommonAncestor_allPath(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        if(!root) return NULL;
+        vector<vector<TreeNode*> > path = binaryTreePaths(root);
+        vector<vector<TreeNode*> > pq={};
+        bool ifbreak = false;
+        for (int i = 0; i < path.size(); i++)
+        {
+            for (int j = 0; j < path[i].size(); j++)
+            {
+                if (path[i][j]->val == p->val || path[i][j]->val == q->val)
+                {
+                    vector<TreeNode*> tmp(path[i].begin(), path[i].begin()+j+1);
+                    if (pq.empty())
+                    {
+                        pq.push_back(tmp);
+                    }
+                    else if((*(pq[0].end()-1))->val!=tmp[tmp.size()-1]->val)
+                    {
+                        pq.push_back(tmp);
+                        ifbreak=true;
+                        break;
+                    }
+                }
+            }
+            if(ifbreak) break;
+        }
+        int i=0,j=0;
+        while (pq[0][i]->val==pq[1][j]->val)
+        {
+            i++;
+            j++;
+        }
+        return pq[0][i-1];
+        
+    }
+    vector<vector<TreeNode*> > binaryTreePaths(TreeNode* root) 
+    {
+        if (root==NULL) return {};
+        if (!root->left && !root->right) return {{root}};
+
+        vector<vector<TreeNode*> > result={};
+        vector<vector<TreeNode*> > left = binaryTreePaths(root->left);
+        vector<vector<TreeNode*> > right = binaryTreePaths(root->right);
+
+        for (int i = 0; i < left.size(); i++)
+        {
+            left[i].insert(left[i].begin(), root);
+            result.push_back(left[i]);
+        }
+        for (int j = 0; j < right.size(); j++)
+        {
+            right[j].insert(right[j].begin(), root);
+            result.push_back(right[j]);
+        }
+
+        return result;      
+    }
+
+    //(2)用递归的思想会更简单一些
+    TreeNode* _236_lowestCommonAncestor_digui(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        if (!root || root==p || root==q) return root;
+        TreeNode* left = _236_lowestCommonAncestor_digui(root->left, p, q);
+        TreeNode* right= _236_lowestCommonAncestor_digui(root->right, p, q);
+        //如果left为空，则返回右边；
+        //如果left不为空，则返回(right == nullptr? left : root)
+        //                      right为空则返回left
+        //                      right不为空，则返回root
+        return left == nullptr? right : (right == nullptr? left : root); 
+    }
+
 };
 
 int main()
 {
     Solution* solution;
-    vector<string> test = {"1", "2", "3", "null", "4"};
-    TreeNode* test_output = solution->initialTreeNode(test);
+    // vector<string> test = {"1", "2", "3", "null", "4"};
+    // TreeNode* test_output = solution->initialTreeNode(test);
 
-    // test 257
-    vector<string> vec_257 = {"1","2","3","null","5"};
-    TreeNode* root_257 = solution->initialTreeNode(vec_257);
-    vector<string> result_257 = solution->_257_binaryTreePaths(root_257);
+    // // test 257
+    // vector<string> vec_257 = {"1","2","3","null","5"};
+    // TreeNode* root_257 = solution->initialTreeNode(vec_257);
+    // vector<string> result_257 = solution->_257_binaryTreePaths(root_257);
 
-    //test 108
-    vector<int> vec_108 = {-10,-3,0,5,9};
-    TreeNode* root_108 = solution->_108_sortedArrayToBST_fast(vec_108);
+    // //test 108
+    // vector<int> vec_108 = {-10,-3,0,5,9};
+    // TreeNode* root_108 = solution->_108_sortedArrayToBST_fast(vec_108);
 
-    //test 404
-    vector<string> vec_404 = {"3","9","20","null","null","15","7"};
-    TreeNode* root_404 = solution->initialTreeNode(vec_404);
-    int result_404 = solution->_404_sumOfLeftLeaves(root_404);
+    // //test 404
+    // vector<string> vec_404 = {"3","9","20","null","null","15","7"};
+    // TreeNode* root_404 = solution->initialTreeNode(vec_404);
+    // int result_404 = solution->_404_sumOfLeftLeaves(root_404);
+
+    //test 236
+    vector<string> vec_236 = {"3","5","1","6","2","0","8","null","null","7","4"};
+    vector<string> p_236 = {"5","6","2","null","null","7","4"};
+    vector<string> q_236 = {"1","0","8"};
+    TreeNode* root_236 = solution->initialTreeNode(vec_236);
+    TreeNode* p_tree = solution->initialTreeNode(p_236);
+    TreeNode* q_tree = solution->initialTreeNode(q_236);
+    TreeNode* result_236 = solution->_236_lowestCommonAncestor_allPath(root_236, p_tree, q_tree);
+
 
     return 1;
 }
