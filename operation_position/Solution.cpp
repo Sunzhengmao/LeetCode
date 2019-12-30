eb454b8bf23b5d96cbb8d8f61a25f3144fb87c28
Sunzhengmao <Sunzhengmao@buaa.edu.cn>
2019-12-31 00:11:34 +0800

    2019.12.30今年的倒数第二天，因为不想错过，所以还是打开了电脑写了一个小题

--- operation_position/Solution.cpp
+++ operation_position/Solution.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
 class Solution
@@ -36,7 +37,7 @@ public:
     }
      
     
-    //尼玛
+    //尼玛，位操作简直神了
     int _136_singleNumber_yihuo(vector<int> & nums)
     {
         int ret = 0;
@@ -47,14 +48,50 @@ public:
         }
         return ret;
     }
+
+//======================================================================================================
+    //169、给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+    int _169_majorityElement_1(vector<int>& nums) 
+    {
+        map<int, int> count={};
+        for (int i = 0; i < nums.size(); i++)
+        {
+            count[nums[i]]++;
+            if(count[nums[i]]>nums.size()/2)
+                return nums[i];
+        }        
+        return 0;
+    }
+
+    int _169_majorityElement_2(vector<int>& nums) 
+    {
+        map<int, int> count={};
+        for (int i = 0; i < nums.size(); i++)
+        {
+            count[nums[i]]++;
+        }        
+        for (map<int, int>::iterator it = count.begin(); it != count.end(); it++)
+        {
+            if (it->second > nums.size()/2)
+            {
+                return it->first;
+            }
+        }
+        return 0;
+    }
 };
 
 int main()
 {
     Solution *solution;
+
     // test 136
     vector<int> nums_136 = {4,1,2,1,2};
     int result_136 = solution->_136_singleNumber_yihuo(nums_136);
+    
+    // test 169
+    vector<int> nums_169 = {3,2,2};
+    int result_169 = solution->_169_majorityElement(nums_169);
 
     return 0;
 }
