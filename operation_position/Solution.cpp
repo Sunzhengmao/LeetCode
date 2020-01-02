a3d59a63dcef0304ba1d5e9bae456875acc15d22
Sunzhengmao <Sunzhengmao@buaa.edu.cn>
2020-01-02 22:41:26 +0800

    2019.1.2一九四位操作

--- operation_position/Solution.cpp
+++ operation_position/Solution.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <bitset>
 using namespace std;
 
 class Solution
@@ -84,8 +85,66 @@ public:
     //461、两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
     int _461_hammingDistance(int x, int y) 
     {
+        int n = x^y;
+        int n_ji = n & 0xAAAAAAAA; //1010 1010 1010 1010
+        int n_ou = n & 0x55555555; //0101 0101 0101 0101
+        n = (n_ji>>1) + n_ou;
+        n_ji = n & 0xCCCCCCCC;     //1100 1100 1100 1100
+        n_ou = n & 0x33333333;
+        n = (n_ji>>2) + n_ou;
+        
+        n_ji = n & 0xF0F0F0F0;
+        n_ou = n & 0x0F0F0F0F;
+        n = (n_ji>>4) + n_ou;
 
-        return 1;
+        n_ji = n & 0xFF00FF00;
+        n_ou = n & 0x00FF00FF;
+        n = (n_ji>>8) + n_ou;
+
+        n_ji = n & 0xFFFF0000;
+        n_ou = n & 0x0000FFFF;
+        return (n_ji>>16) + n_ou;
+    }
+
+//=========================================================================================================
+    //输出一个二进制数字
+    template <class T>  
+    void PrintfBinary(T a)  
+    {   
+        for (int i = sizeof(a) * 8 - 1; i >= 0; --i)  
+        {  
+            if ((a >> i) & 1)  
+                putchar('1');  
+            else   
+                putchar('0');  
+            if (i % 4 == 0)  
+                putchar(' ');  
+        }  
+        putchar('\n');  
+    } 
+
+//=========================================================================================================
+    //268、给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+    int _268_missingNumber_shabi(vector<int>& nums) 
+    {
+        int sum = 0;
+        int i = 0;
+        for (; i < nums.size(); i++)
+        {
+            sum+=nums[i]-i;
+        }
+        return i-sum;        
+    }
+
+    int _268_missingNumber_operator(vector<int>& nums)
+    {
+        int result = nums.size();
+        for (int i = 0; i < nums.size(); i++)
+        {
+            result^=nums[i];
+            result^=i;
+        }   
+        return result;
     }
 };
 
@@ -121,6 +180,51 @@ int main()
     uint16_t m_high = m>>8;
     uint16_t m_low = m<<8;
     cout<< (m_high | m_low) <<endl;
+    cout<< sizeof(int) <<endl;
+    cout<< sizeof(m) <<endl;
+    cout<< sizeof(uint16_t) <<endl;
+
+    // 6、二进制逆序
+    uint16_t n = 34520;
+    solution->PrintfBinary(n);
+    // cout<<bitset<sizeof(n)*8>(n)<<endl;这也是输出二进制数字的方法，但是没有空格
+    uint16_t n_ji = n & 0xAAAA;//1010 1010 1010 1010只取了奇数位
+    uint16_t n_ou = n & 0x5555;//0101 0101 0101 0101只取了偶数位
+    n = (n_ji>>1) | (n_ou<<1); //实现了每2位为一组的交换
+    n_ji = n & 0xCCCC;         //1100 1100 1100 1100
+    n_ou = n & 0x3333;         //0011 0011 0011 0011
+    n = (n_ji>>2) | (n_ou<<2); //实现了每4位为一组的交换
+    n_ji = n & 0xF0F0;         //1111 0000 1111 0000
+    n_ou = n & 0x0F0F;         //0000 1111 0000 1111
+    n = (n_ji>>4) | (n_ou<<4); //实现了每8位为一组的交换
+    n_ji = n & 0xFF00;         //1111 1111 0000 0000
+    n_ou = n & 0x00FF;         //0000 0000 1111 1111
+    n = (n_ji>>8) | (n_ou<<8);
+    cout<< n <<endl;
+
+    // 7、二进制数1的个数
+    uint16_t g = 34520;
+    solution->PrintfBinary(g);
+    uint16_t g_ji = g & 0xAAAA; //1010 1010 1010 1010
+    uint16_t g_ou = g & 0x5555; //0101 0101 0101 0101
+    g = (g_ji>>1) + g_ou;
+    solution->PrintfBinary(g);
+    g_ji = g & 0xCCCC;          //1100 1100 1100 1100
+    g_ou = g & 0x3333;          //0011 0011 0011 0011
+    g = (g_ji>>2) + g_ou;
+    solution->PrintfBinary(g);
+    g_ji = g & 0xF0F0;          //1111 0000 1111 0000
+    g_ou = g & 0x0F0F;          //0000 1111 0000 1111
+    g = (g_ji>>4) + g_ou;
+    solution->PrintfBinary(g);
+    g_ji = g & 0xFF00;          //1111 1111 0000 0000
+    g_ou = g & 0x00FF;          //0000 0000 1111 1111
+    g = (g_ji>>8) + g_ou;
+    solution->PrintfBinary(g);
+    cout<< g <<endl;
+
+
+
     //======================================//
 
 
