3f87d1d8085ec1a6ebb45915c67cf56fc2850b01
Sunzhengmao <Sunzhengmao@buaa.edu.cn>
2020-02-06 23:22:47 +0800

    2020/2/6

--- dynamic Programming/Solution.cpp
+++ dynamic Programming/Solution.cpp
@@ -12,6 +12,7 @@ private:
 public:
     Solution(/* args */);
     ~Solution();
+
 // 746. 使用最小花费爬楼梯,爬不需要费体力，停下来需要消耗体力
     int _746_minCostClimbingStairs(vector<int>& cost) 
     {
@@ -50,6 +51,61 @@ public:
         return min(c1, c2);
     }
 
+//5、给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+    string _5_longestPalindrome(string s) 
+    {
+        const int N = s.size();
+        if(N <= 1) return s;
+
+        int maxLen_ = 0;
+        string maxPali = "";
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = N-1; j >= i; j--)
+            {
+                if (j-i+1 < maxLen_)
+                    continue;
+                if(isPalindrome(s, i, j))
+                {
+                    if (j-i+1 > maxLen_)
+                    {
+                        maxLen_ = j-i+1;
+                        maxPali = strcopy(s,i,j+1);  
+                    }
+                }      
+            }
+        }
+        return maxPali;        
+    }
+
+    bool isPalindrome(string& s, int begin, int end)
+    {
+        if (end < begin)  return false;
+
+        while (end > begin) 
+        {
+            if (s[begin]!=s[end])
+                return false;
+
+            end--;
+            begin++;
+        }
+        return true;
+    }
+    string strcopy(string& src, int begin, int end)
+    {
+        if (end < begin) return "";
+
+        string result = "";
+        
+        for (int i = begin; i < end; i++)
+        {
+            result.push_back(src[i]);
+        }
+
+        return result;
+    }
 };
 
 
@@ -61,6 +117,9 @@ int main()
     //test 746
     vector<int> cost_746 = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
     int result_746 = solution->_746_minCostClimbingStairs_key(cost_746);
+
+    string s_5 = "cbbd";
+    string result_5 = solution->_5_longestPalindrome(s_5);
 
     return 1;
 }
