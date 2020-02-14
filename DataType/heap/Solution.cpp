9efc67866c41321ba16f6f1aa6efbe17bba2e75a
Sunzhengmao <Sunzhengmao@buaa.edu.cn>
2020-02-14 23:40:27 +0800

    enen

--- DataType/heap/Solution.cpp
+++ DataType/heap/Solution.cpp
@@ -11,6 +11,81 @@ private:
 public:
     Solution(/* args */);
     ~Solution();
+    //--------------| MOOC heap |----------------//
+    void _mooc_generateMinHeap(vector<int>& nums)
+    {
+        if (nums.size() <= 1) return ;
+
+        const int N = nums.size();
+        int layer = log2(N);
+
+        while (layer > 0)
+        {
+            int start = 2 ^ layer - 1;
+            int end = min(N, 2^(layer+1)-1);
+
+            for (int i = start; i < end; i++)
+                _mooc_ShiftUpMinHeap(nums, i);
+
+            layer--;            
+        }
+    }
+
+    void _mooc_ShiftDownMinHeap(vector<int>& nums, int position)
+    {
+        const int N = nums.size();
+
+        int left = 2 * position + 1;
+        int right = 2 * position + 2;
+
+        while (N-1 >= left) 
+        {
+            int min_index = nums[position] < nums[left] ? position : left;
+            
+            if (N-1 >= right)
+            {
+                min_index = nums[min_index] < nums[right] ? min_index : right;
+            }
+
+            swap(nums[position], nums[min_index]);
+
+            position = min_index;
+
+            left = 2 * position + 1;
+            right = 2 * position + 2;
+        }
+    }
+
+    void _mooc_ShiftUpMinHeap(vector<int>& nums, int position)
+    {
+        const int N = nums.size();
+
+        while (position > 0)
+        {
+            int fatherPos = position - 1 >> 1;
+
+            if (nums[fatherPos] >  nums[position])
+            {
+                swap(nums[fatherPos], nums[position]);
+            }
+            position = fatherPos;
+        }
+    }
+
+    void _mooc_AddMinHeap(vector<int>& nums, int value)
+    {
+        nums.push_back(value);
+        _mooc_ShiftUpMinHeap(nums, nums.size());
+    }
+
+    void _mooc_DeleteMinHeap(vector<int>& nums, int position)
+    {
+        const int N = nums.size();
+        if (position < 0 || position >= N) return ;
+        swap(nums[position], nums[N-1]);
+        nums.resize(N-1);
+        _mooc_ShiftDownMinHeap(nums, position);
+    }
 
 //=======================================================================================================
     //1046、有一堆石头，每块石头的重量都是正整数。每次选出两块最重的石头将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。
